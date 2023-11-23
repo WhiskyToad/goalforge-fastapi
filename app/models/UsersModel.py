7274b159894c5e36409f86542f8aeac1e7ab3684
Steven Craig <stevencraig321@gmail.com>
2023-11-23 21:37:25 +0000

    refacot: entity meta

--- app/models/UsersModel.py
+++ app/models/UsersModel.py
@@ -1,10 +1,9 @@
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
+from models.BaseModel import EntityMeta
 
-Base = declarative_base()
 
-
-class UserModel(Base):
+class UserModel(EntityMeta):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
--- migrations/env.py
+++ migrations/env.py
@@ -8,7 +8,7 @@ from sqlmodel import SQLModel
 
 from alembic import context
 
-from app.models.UsersModel import UserModel
+from app.models.BaseModel import EntityMeta
 
 
 # this is the Alembic Config object, which provides
@@ -24,7 +24,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = UserModel.metadata
+target_metadata = EntityMeta.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
